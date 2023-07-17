819dac1e926632a9931347a15c4481a7ca16ff6a
dreksha <dreksha.chaudhary@team.telstra.com>
2023-07-17 23:54:25 +0530

    first commit to thread

---
+++ src/main/java/org/example/PreventThreadExcution.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class PreventThreadExcution
+{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //Step1
+        //We can preven Thread Excution By following method
+            //Yilead method
+            //join method
+            //sleep
+
+        //Yilead Method
+    }
+}
+
